bcfadcf495c85f97eff3440d8046357b93d4c228
AsimAdam <93882610+AsimAdam@users.noreply.github.com>
2024-06-14 16:48:58 +0400

    latest changes

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Enable All CORS operations
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+}));
+
+let placeholderText = "Type Your Name";
+let code = "red";
+
+app.use((req, res, next) => {
+    console.log(`${req.method} request for '${req.url}'`);
+    next();
+});
+
+app.get('/placeholder', (req, res) => {
+    console.log('getPlaceholderState called');
+    console.log(`Current code: ${code}`);
+    const responseText = code === "green" ? placeholderText : "";
+    console.log(`Returning placeholderText: ${responseText}`);
+    res.json({ code, placeholderText: responseText });
+});
+
+// Toggle endpoint to change the code state
+app.get('/toggle', (req, res) => {
+    console.log('toggle called');
+    code = code === "green" ? "red" : "green";
+    console.log(`Toggled code to: ${code}`);
+    res.json({ code });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
